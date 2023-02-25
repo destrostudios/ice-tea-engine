f630fc847ae072bf8441e6136c585c7014c9f2a3
carlkuesters <carl.kuesters@youengineering.com>
2023-02-25 18:31:58 +0100

    Change shadow map default depth format from 16 to 32 bit.

--- ice-tea-engine-core/src/main/java/com/destrostudios/icetea/core/render/shadow/ShadowConfig.java
+++ ice-tea-engine-core/src/main/java/com/destrostudios/icetea/core/render/shadow/ShadowConfig.java
@@ -3,13 +3,13 @@ package com.destrostudios.icetea.core.render.shadow;
 import lombok.Getter;
 import lombok.Setter;
 
-import static org.lwjgl.vulkan.VK10.VK_FORMAT_D16_UNORM;
+import static org.lwjgl.vulkan.VK10.VK_FORMAT_D32_SFLOAT;
 
 @Getter
 @Setter
 public class ShadowConfig {
 
-	private int shadowMapFormat = VK_FORMAT_D16_UNORM;
+	private int shadowMapFormat = VK_FORMAT_D32_SFLOAT;
 	private int shadowMapSize = 4096;
 	private float brightness = 0.1f;
 	private int cascadesCount = 4;
